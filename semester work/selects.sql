9fa58ebd4c126b9d8626fa09dc6d143cf7af099c
bondaxme <maksbond280104@gmail.com>
2023-01-23 15:00:41 +0200

    semester work selects in progress

--- semester work/selects.sql
+++ semester work/selects.sql
@@ -19,6 +19,27 @@ order by appointment_date;
 
 select * from appointment_data;
 
+drop view if exists patient_rooms;
+create or replace view patient_rooms as
+select end_date, patient_name, room_number, type
+from room_housing
+join patient p on room_housing.patient_id = p.id
+join room r on room_housing.room_id = r.id
+where end_date > curdate()
+order by end_date;
+
+select * from patient_rooms;
+
+select room_number, type, count(*) as patients_amount
+from patient_rooms
+group by room_number, type;
+
 select doctor_name, count(*) as appointments_amount, sum(price) as earned_money, service_name
 from appointment_data
 group by doctor_name, service_name;
+
+select patient_name, payment_date, payment_method, sum(amount) as amount_of_payments
+from patient
+left join appointment a on patient.id = a.patient_id
+left join payment p on a.payment_id = p.id
+group by patient_name, payment_date, payment_method;
