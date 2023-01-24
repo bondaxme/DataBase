8bbe6992dce8934f21758939ffc41931f12342d7
bondaxme <maksbond280104@gmail.com>
2023-01-24 21:20:24 +0200

    semester work selects in progress

--- semester work/selects.sql
+++ semester work/selects.sql
@@ -1,6 +1,6 @@
 use hospital;
 
-#general data about patients
+#1 general data about patients
 drop view if exists patient_data;
 create or replace view patient_data as
 select patient_name, sex, height, weight, blood_type, allergies, medical_history
@@ -9,7 +9,7 @@ join medical_card mc on patient.id = mc.patient_id;
 
 select * from patient_data;
 
-#general data about appointments
+#2 general data about appointments
 drop view if exists appointment_data;
 create or replace view appointment_data as
 select appointment_date,doctor_name, speciality, patient_name, service_name, duration_minutes, price
@@ -21,7 +21,7 @@ order by appointment_date;
 
 select * from appointment_data;
 
-#room where lives every patient
+#3 room where lives every patient
 select end_date, patient_name, room_number, type
 from room_housing
 join patient p on room_housing.patient_id = p.id
@@ -29,7 +29,7 @@ join room r on room_housing.room_id = r.id
 where curdate() between start_date and end_date
 order by end_date;
 
-#amount of payments from every patient
+#4 amount of payments from every patient
 select patient_name, payment_method, sum(amount) as amount_of_payments
 from patient
 left join appointment a on patient.id = a.patient_id
@@ -37,20 +37,20 @@ left join payment p on a.payment_id = p.id
 group by patient_name, payment_method
 order by amount_of_payments desc;
 
-#clients and medications prescribed to them
+#5 clients and medications prescribed to them
 select patient_name, medication_name, dosage_in_mg, price
 from patient
 left join appointment a on patient.id = a.patient_id
 left join medication_appointment ma on a.id = ma.appointment_id
 left join medication m on ma.medication_id = m.id;
 
-#pateints who have medical insurance
+#6 pateints who have medical insurance
 select patient_name
 from patient
 join medical_card on patient.id = medical_card.patient_id
 WHERE medical_card.availability_of_insurance = 1;
 
-#last week appointments
+#7 last week appointments
 select service_name, doctor_name, appointment_date
 from appointment
 join service on appointment.service_id = service.id
@@ -58,7 +58,7 @@ join doctor on appointment.doctor_id = doctor.id
 where appointment_date between DATE_ADD(curdate(), INTERVAL -7 DAY) and curdate()
 order by appointment_date;
 
-#amount of money was earned by doctors
+#8 amount of money was earned by doctors
 select doctor_name, count(*) as appointments_amount, sum(price) as earned_money, service_name
 from doctor
 join appointment a on doctor.id = a.doctor_id
@@ -66,46 +66,47 @@ join service s on a.service_id = s.id
 group by doctor_name, service_name
 order by earned_money desc;
 
-#amount of patients in every room
+#9 amount of patients in every room
 select room_number, type, count(*) as patients_amount
 from room_housing
 join room r on room_housing.room_id = r.id
 group by room_number, type;
 
-#amount of prescriptions for every medication
+#10 amount of prescriptions for every medication
 select medication_name, dosage_in_mg, count(*) as prescription
 from medication_appointment
 join medication m on medication_appointment.medication_id = m.id
 group by medication_name, dosage_in_mg;
 
-#medications need to be ordered and amount to order
+#11 medications need to be ordered and amount to order
 select medication_name, count(*) as required_amount
 from medication_appointment
 join medication m on m.id = medication_appointment.medication_id
 where availability = 0
 group by medication_name;
 
-#count the amount of patients for every doctor
+#12 count the amount of patients for every doctor
 select doctor_name, speciality, count(distinct patient_id) as unique_patients
 from doctor
 left join appointment a on doctor.id = a.doctor_id
 group by doctor_name, speciality;
 
-#patients without allergies and medical history
+#13 patients without allergies and medical history
 select patient_name
 from patient
 join medical_card mc on patient.id = mc.patient_id
 where allergies is null and medical_history is null;
 
-#patients who have AB+ blood type and can be universal recipient
+#14 patients who have AB+ blood type and can be universal recipient
 select patient_name
 from patient
 join medical_card mc on patient.id = mc.patient_id
 where blood_type = 'AB+';
 
-select round(avg(med_price + service_price), 2) as avg_appointment_price, max(med_price + service_price) as max_appointment_price, min(med_price + service_price) as min_appointment_price
-from (select appointment.id, m.price as med_price, s.price as service_price
-        from appointment
-        left join service s on appointment.service_id = s.id
-        left join medication_appointment ma on appointment.id = ma.appointment_id
-        left join medication m on ma.medication_id = m.id) as prices;
+#15 prices per every appointment
+select appointment.id, m.price as med_price, s.price as service_price
+from appointment
+left join service s on appointment.service_id = s.id
+left join medication_appointment ma on appointment.id = ma.appointment_id
+left join medication m on ma.medication_id = m.id
+order by id;
