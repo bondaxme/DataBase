f7930fb32a8600df562907d23235b415122c742e
bondaxme <maksbond280104@gmail.com>
2023-01-25 12:40:45 +0200

    index added

--- semester work/test.sql
+++ semester work/test.sql
@@ -1,5 +1,13 @@
 use hospital;
 
+explain
+select service_name
+from service
+where price = 15499;
+
+create index test
+on service(price);
+
 insert into room_housing(id, patient_id, room_id, start_date, end_date) value (26,19,3,'2023-01-25','2023-01-30');
 
 update medical_card
